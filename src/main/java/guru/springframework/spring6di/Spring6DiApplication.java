5e4edc18cce9ab302bbcbdb5b73e57c9ee2f3c1a
Faintful <72162133+Faintful@users.noreply.github.com>
2024-01-25 21:21:19 +0100

    Added fake controller and application context configuration in main class

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -1,13 +1,19 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(Spring6DiApplication.class, args);
+
+        MyController myController = context.getBean(MyController.class);
+
+        System.out.println(myController.sayHello());
     }
 
 }
