15c02166dc8f2bff90480bdd93d870f86dfe4c03
Faintful <72162133+Faintful@users.noreply.github.com>
2024-01-29 21:12:22 +0100

    Added a second implementation  to GreetingService to demonstrate @Primary behavior

--- src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
+    // Since there is only one constructor in this class, and the class has been autowired where it is used in ConstructorInjectedControllerTest,
+    // this constructor does not need an @Autowired at the class level, as Spring realizes that there is only one way for this class to be instantiated,
+    // and that is via this constructor. This will work. However, @Autowired can still be applied at this level for consistency.
+
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
